d36b8109bbc10d607718bffed0d90507f66eefac
Tomas Beuzen <tomas.beuzen@stat.ubc.ca>
2021-09-06 11:12:13 +1000

    cookiecutter has moved

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,6 +4,6 @@ os.chdir("..")
 shutil.rmtree("bar")
 
 print(
-    "This cookiecutter has moved. Please use:\n\n"
+    "\nThis cookiecutter has moved. Please use:\n\n"
     "\tcookiecutter https://github.com/py-pkgs/py-pkgs-cookiecutter.git"
 )
